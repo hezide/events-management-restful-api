30e83471116dd58f34b80555b45803edbc887892
hezideb <88391844+hezideb@users.noreply.github.com>
2023-12-22 15:07:56 +0200

    Define DB connection

--- events-management-restful-api/src/main/java/com/alfabet/eventsmanagementrestfulapi/model/Event.java
+++ events-management-restful-api/src/main/java/com/alfabet/eventsmanagementrestfulapi/model/Event.java
@@ -1,4 +1,11 @@
 package com.alfabet.eventsmanagementrestfulapi.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "events")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 }
